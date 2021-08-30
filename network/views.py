a20c92873a1746cb4dfa66154d4ba5135dcf0cfe
Pedro <theskorpions2014@gmail.com>
2021-08-30 21:18:26 +0300

    Fix

--- network/views.py
+++ network/views.py
@@ -52,7 +52,7 @@ class HomePage(ListView):
 
 class UserPage(DetailView):
     model = User
-    template_name = 'PostMetaAnonymous/profile/profile.html'
+    template_name = 'PostMetaAnonymous/Profile/profile.html'
     context_object_name = 'user'
 
     def get_object(self, queryset=None):
